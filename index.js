39f11a3cfedaa7feeeda4a8a3b05bfcea3780062
Jardeylson jacinto <jardeylsong.m@gmail.com>
2022-08-27 16:26:38 -0300

    add Data base

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 dotenv.config()
 
 app.use('/static', express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+
+//connection to db
+mongoose.set('userFindAndModify', false)
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParse: true }, () => {
+  console.log('connected to db!')
+
+  app.listen(3333, () => console.log('Serve up and running'))
+})
 
 //view engine configuration
 app.set('view engine', 'ejs')
